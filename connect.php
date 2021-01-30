bb1ffcd03db32dfed8993c90f3a915b41e93a787
Gourangkumrawat <69038102+Gourangkumrawat@users.noreply.github.com>
2021-01-30 23:21:35 +0530

    Add files via upload

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-	//$firstName = $_POST['fullName'];
-	//$email = $_POST['email'];
-	//$address = $_POST['address'];
-	//$number = $_POST['number'];
+	$firstName = $_POST['fullName'];
+	$email = $_POST['email'];
+	$address = $_POST['address'];
+	$number = $_POST['number'];
 
 	// Database connection
 	$conn = new mysqli('localhost','root','','register');
@@ -10,7 +10,7 @@
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
 	} else {
-		$stmt = $conn->prepare("insert into register(fullName, email, address, number) values("gourang", "abc", "ghh", "1234")");
+		$stmt = $conn->prepare("insert into register(fullName, email, address, number) values(?, ?, ?, ?)");
 		$stmt->bind_param("sssi", $fullName, $email, $address, $number);
 		$execval = $stmt->execute();
 		echo $execval;
